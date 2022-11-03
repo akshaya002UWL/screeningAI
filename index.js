bcd70e7e531d0fba744dbf1a7b3195b013a0b0f7
akshaya002UWL <104349647+akshaya002UWL@users.noreply.github.com>
2022-11-04 00:41:29 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,46 @@
+var express = require("express"); //requiring express module
+var app = express(); //creating express instance
+var querystring = require('querystring');
+const axios = require('axios');
+
+
+
+
+var data = querystring.stringify({
+    grant_type: "client_credentials",
+    client_id: "watson-orchestrate",
+    client_secret:"ca81109d-312d-4ed3-9cf0-19398e26ea9d"
+  });
+
+app.get('/screeningAI',async (req,res) => {
+
+    
+
+try {
+    const jwt_token= await axios.post('https://keycloak-edu-keycloak.apps.openshift-01.knowis.cloud/auth/realms/education/protocol/openid-connect/token',data,{
+          headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': Buffer.byteLength(data)
+        }
+    }
+    );
+    const axiosInstance = axios.create({
+        headers: { 
+            'Authorization': 'Bearer '+jwt_token.data.access_token
+        }
+    });
+    const response = await axiosInstance.get('https://education-dev.apps.openshift-01.knowis.cloud:443/screenedcandidates/api/screen/screeningAI');
+    res.send(response.data)
+} catch (error) {
+    console.log(error);
+}
+
+        });
+
+
+app.listen(3000, function() {
+ console.log("Node server is running..");
+});
+
+
+
